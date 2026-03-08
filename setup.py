f1ea134e0e58d2002c4c8d38318c3eef35f064cd
Andreas Legner <hi@legner.beer>
2026-03-08 22:08:52 +0100

    fixxes Docs Test

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ with open("README.md", "r", encoding = "utf-8") as fh:
 
 setup(  # Note: no 'setuptools.' prefix here
     name = "iLibrary",
-    version = "0.0.14",
-    author = "jgh1",
+    version = "0.0.15",
+    author = "legner.beer",
     author_email = "iLibrary@legner.beer",
     description = "iLibrary - Tools for IBM i library",
     long_description = long_description,
