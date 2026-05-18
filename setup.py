6b3a3e87b084d86f441531edb97d8934b1f06c64
Andreas Legner <hi@legner.beer>
2026-05-18 13:54:06 +0200

    set up to v0.0.18

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding = "utf-8") as fh:
 
 setup(  # Note: no 'setuptools.' prefix here
     name = "iLibrary",
-    version = "0.0.17",
+    version = "0.0.18",
     author = "legner.beer",
     author_email = "iLibrary@legner.beer",
     description = "iLibrary - Tools for IBM i library",
@@ -13,7 +13,7 @@ setup(  # Note: no 'setuptools.' prefix here
     long_description_content_type = "text/markdown",
     url = "https://legner.beer",
     project_urls = {
-        "Bug Tracker": "https://legner.beer/bugs",
+        "Bug Tracker": "https://github.com/legnerbeer/iLibrary/issues",
         "Git Hub": "https://github.com/jgh1/iLibrary"
     },
     classifiers = [
